ab83e82e88c69fdc703c9e11ce2201f993a91391
Ann1-mj <aj2615@srmist.edu.in>
2026-04-13 15:49:45 +0530

    uc5

---
+++ src/UC5.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashSet;
+
+public class UC5 {
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Formation using LinkedHashSet ===");
+
+        // Create LinkedHashSet
+        LinkedHashSet<String> train = new LinkedHashSet<>();
+
+        // Add bogies
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("Cargo");
+        train.add("Guard");
+
+        // Attempt duplicate
+        train.add("Sleeper");
+
+        // Display final formation
+        System.out.println("Final train formation: " + train);
+    }
+}
